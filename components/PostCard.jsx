26eb26c6d3d65b0cf03ec590336235d7060f09a9
Jake Tyler Simon <jtsimonbaseball@gmail.com>
2022-02-14 12:50:27 -0800

    update to JS

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import moment from 'moment';
+import Link from 'next/link';
 
 const PostCard = ({post}) => {
   return (
-    <div>
-        {post.title}
-        {post.excerpt}
+    <div className='bgwhite shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+        <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+            {/* <img src={post.featuredImage.url} alt='post.title' className='object-top absolute h-80 w-ful object-cover shadow-lg rounded-t-lg lg:rounded-lg'/> */}
+        </div>
     </div>
   )
 }
---
+++ services/index.js
@@ -0,0 +1,40 @@
+import {request, gql} from 'graphql-request';
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINTS;
+
+export const getPosts = async () => {
+    const query = gql`
+        query MyQery {
+                postsConnection {
+                  edges {
+                    node {
+                      author {
+                        bio
+                        id
+                        name
+                        photo {
+                          url
+                        }
+                      }
+                      createdAt
+                      slug
+                      title
+                      excerpt
+                      featuredImage {
+                        url
+                      }
+                      categories {
+                        name
+                        slug
+                      }
+                    }
+                  }
+                }
+              
+        }
+    `;
+
+    const result = await request(graphqlAPI, query);
+
+    return result.postsConnection.edges;
+};
