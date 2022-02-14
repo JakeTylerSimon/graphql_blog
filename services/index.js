1f827a257e8a2c278953375b9ffb214112b8be20
Jake Tyler Simon <jtsimonbaseball@gmail.com>
2022-02-14 13:22:52 -0800

    fixed bugs

--- services/index.js
+++ services/index.js
@@ -4,34 +4,33 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINTS;
 
 export const getPosts = async () => {
     const query = gql`
-        query MyQery {
-                postsConnection {
-                  edges {
-                    node {
-                      author {
-                        bio
-                        id
-                        name
-                        photo {
-                          url
-                        }
-                      }
-                      createdAt
-                      slug
-                      title
-                      excerpt
-                      featuredImage {
-                        url
-                      }
-                      categories {
-                        name
-                        slug
-                      }
-                    }
-                  }
+    query MyQuery {
+        postsConnection {
+          edges {
+            node {
+              author {
+                bio
+                id
+                name
+                photo {
+                  url
                 }
-              
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
         }
+      }
     `;
 
     const result = await request(graphqlAPI, query);
