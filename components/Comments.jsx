d323efd055e773677ca51de43ed18d9112c54bdd
Jake Tyler Simon <jtsimonbaseball@gmail.com>
2022-02-18 14:57:32 -0800

    update

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import momen from 'moment'
+import parse from 'html-react-parser'
 
-const Comments = () => {
+const Comments = ({ slug }) => {
+  
+  const [comments, etComments] = useState([]);
+
+  useEffect(() => {
+    getComments(slug)
+  }, [])
+  
   return (
     <div>Comments</div>
   )
--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,13 +1,13 @@
-import {GraphQLClient, gql} from 'graphql'
+import { GraphQLClient, gql } from 'graphql-request';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export default function comments(req, res) {
-  const graphQlClient = new GraphQLClient(graphqlAPI, {
-    header: {
-      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}` 
-    }
-  })
+export default async function asynchandler(req, res) {
+  const graphQLClient = new GraphQLClient((graphqlAPI), {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+  });
 
   const query = gql`
     mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
