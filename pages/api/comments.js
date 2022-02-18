29c00b4b61b68567dcb80e5454d749be6a52e5ef
Jake Tyler Simon <jtsimonbaseball@gmail.com>
2022-02-17 18:02:16 -0800

    update

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,5 +1,26 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {GraphQLClient, gql} from 'graphql'
 
-export default function helloAPI(req, res) {
-  res.status(200).json({name: 'John Doe'})
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export default function comments(req, res) {
+  const graphQlClient = new GraphQLClient(graphqlAPI, {
+    header: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}` 
+    }
+  })
+
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) { id }
+    }
+  `;
+
+  const result = await graphQLClient.request(query, {
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment,
+    slug: req.body.slug,
+  });
+
+  return res.status(200).send(result);
 }
